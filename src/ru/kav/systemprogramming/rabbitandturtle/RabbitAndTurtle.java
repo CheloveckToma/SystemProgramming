6de9d7ecd5806201d11f99a57f716ba0a5b0e4ae
CheloveckToma <ev83RUS@gmail.com>
2019-10-23 10:57:46 +0300

    Распределил методы по классам, сделал "нормальную" логическую стркутуру

--- src/ru/kav/systemprogramming/rabbitandturtle/RabbitAndTurtle.java
+++ src/ru/kav/systemprogramming/rabbitandturtle/RabbitAndTurtle.java
@@ -2,8 +2,8 @@ package ru.kav.systemprogramming.rabbitandturtle;
 
 public class RabbitAndTurtle {
     public static void main(String[] args) {
-        Thread turtle = (new AnimalThread("turtle", 1));
-        Thread rabbit = (new AnimalThread("rabbit", 10));
+        Thread turtle = new AnimalThread("turtle", 1);
+        Thread rabbit = new AnimalThread("rabbit", 10);
         turtle.start();
         rabbit.start();
     }
