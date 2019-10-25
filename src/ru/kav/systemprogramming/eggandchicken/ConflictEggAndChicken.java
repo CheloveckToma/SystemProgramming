0ebadba99aa815dfed8331077fa81853cdc31472
CheloveckToma <ev83RUS@gmail.com>
2019-10-25 11:11:28 +0300

    Распределил методы по классам, сделал "нормальную" логическую стркутуру

---
+++ src/ru/kav/systemprogramming/eggandchicken/ConflictEggAndChicken.java
@@ -0,0 +1,23 @@
+package ru.kav.systemprogramming.eggandchicken;
+
+public class ConflictEggAndChicken implements Runnable {
+
+    Thread thread;
+
+    public ConflictEggAndChicken(String name) {
+        thread = new Thread(this, name);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(thread.getName());
+        }
+    }
+}
