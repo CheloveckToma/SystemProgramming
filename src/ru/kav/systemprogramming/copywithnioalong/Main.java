4e9b67e140d71067edbc8d6f3f3ddd43186c6756
CheloveckToma <ev83RUS@gmail.com>
2019-10-29 12:04:02 +0300

    ..

--- src/ru/kav/systemprogramming/copywithnioalong/Main.java
+++ src/ru/kav/systemprogramming/copywithnioalong/Main.java
@@ -1,12 +1,14 @@
 package ru.kav.systemprogramming.copywithnioalong;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        long firstTime = System.currentTimeMillis();
         Thread firstThread = new CopyNIO("src\\\\ru\\\\kav\\\\systemprogramming\\\\copywithnio\\\\fileForWriteOne.txt");
         Thread secondThread = new CopyNIO("src\\\\ru\\\\kav\\\\systemprogramming\\\\copywithnio\\\\fileForWriteTwo.txt");
-
-
+        firstThread.join();
+        secondThread.join();
+        long secondTime = System.currentTimeMillis();
+        System.out.println(secondTime - firstTime);
 
     }
 }
